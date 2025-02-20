0c9f03bb2707a20ce29bb7bb0e61e8717ea1d0e5
shofiq18 <shafiqulislam25021998@gmail.com>
2025-02-20 13:17:32 +0600

    added first comment

---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Navbar from '../Shared/Navbar';
+import Footer from '../Shared/Footer';
+
+const MainLayout = () => {
+    return (
+        <div>
+            <Navbar />
+            <Outlet />
+            <Footer />
+        </div>
+    );
+};
+
+export default MainLayout;
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='container mx-auto min-h-screen'>
+            <h1>Home</h1>
+        </div>
+    );
+};
+
+export default Home;
